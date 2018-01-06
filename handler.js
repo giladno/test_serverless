3a17c64d9464ee20406273d9e1ad94b48dffa874
Gilad Novik <gilad@novik.ca>
2018-01-06 16:46:58 +0200

    proper input/output handling

--- handler.js
+++ handler.js
@@ -1,7 +1,14 @@
 'use strict';
-const handler = handler=>(event, context, callback)=>handler(event, context)
-    .then(res=>callback(null, res))
-    .catch(callback);
+const handler = handler=>(event, context, callback)=>{
+    context.callbackWaitsForEmptyEventLoop = false;
+    handler(event, context)
+        .then(res=>callback(null, res))
+        .catch(err=>callback(null, {
+            statusCode: err.statusCode||500,
+            headers: {'Content-Type': 'text/plain'},
+            body: 'Server Error',
+        }));
+};
 
 module.exports.hello = handler(async (event, context)=>{
     const response = {
